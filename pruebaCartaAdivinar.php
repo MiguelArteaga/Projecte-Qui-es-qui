8f6f985fe9ebc8f81fa498c4942ba94340d73414
miguelarteaga <marteagavalle@gmail.com>
2018-10-17 20:27:37 +0200

    CommitTofo

--- pruebaCartaAdivinar.php
+++ pruebaCartaAdivinar.php
@@ -25,26 +25,38 @@
 	# Condiciones
 	foreach ($arrayImg as $key => $i) {
 	  #$names = explode(":",$i);
-	  $names2 = explode(",",$i);
+	  $names2 = explode(":",$i);
 	  array_push($arrayGeneral, $names2);
 
 	}
 	 #print_r($arrayGeneral);
 	?>
 	<script type="text/javascript" >
-		
+
+
+		//Pasamos $arrayGeneral a arraycartas para poder trabajar con el en Javascript.
 	   	var arraycartas=<?php echo json_encode($arrayGeneral);?>;
 		var arraynombres=[];
+
+
+		//hacemos un bucle para coger solo los nombres de las carta y las guardamos en un array nuevo llamado "arraynombres".
 		for(var i=0;i<arraycartas.length;i++){
 			arraynombres.push(arraycartas[i][0]);}
-			document.write(arraynombres);
+			//document.write(arraynombres);
+		//Creamos la funcion adivinarCarta.
+
 
 	    function adivinaCarta() {
+	    	//Declaramos un variable para hacer 7 numeros random.
 	    	var i=Math.floor((Math.random() * 7));
+
+	    	//Declaramos una variable nueva en la que guardamos la carta a adivinar.
 			var cartaAdivinar=arraynombres[i];
 			//document.write(cartaAdivinar+"<br>");
+			
+			//Eliminamos
 			delete arraynombres[i];
-			//var arraynombres=arraynombres;
+			
 			//document.write(arraynombres);
 
 			for (var i=0 ;i <= arraynombres.length; i++) {
@@ -61,12 +73,9 @@
 		        arrayMezclar[j] = k;
 		    }
 		    for (i=0;i<=arrayMezclar.length;i++){
-		    	 document.write(arraynombres[arrayMezclar[i]]);
+		    	 document.write("<br>"+arraynombres[arrayMezclar[i]]);
 		    }
-		   
-
-
-	    	}
+	    }
 
 	</script>
 
